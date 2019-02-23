a5c5679a81ca451d7767dc61aa6aa7ea9ba6fd75
zwang198 <zwang198@uiowa.edu>
2019-02-23 17:10:37 -0600

    frontend login

--- Group7/login.php
+++ Group7/login.php
@@ -1,15 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>group7</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- frontend work to make it looks better 
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script> 
+    -->
+</head>
 <?php
     require "header.php"
 ?>
     <main>
-      <div>
+    <div class="container">
          <section>
             <h1>Login</h1>
-            <form action = "login.inc.php" method = "post">
-               <input type="text" name="mail" placeholder="email">
-               <input type="password" name="pwd" placeholder="Password">
-               <button type="submit" name="login-submit">Log In</button>
-         </section>
+            <form action = "login.php" method = "post">
+            <div class="form-group"><input type="text" name="mail" placehold="email"></div>
+            <div class="form-group"><input type="password" name="pwd" placehold="Password"></div>
+            <div class="form-group"><button type="submit" name="login-submit">Log In</button></div>
+         </section></div>
       </div>
     </main>
     
