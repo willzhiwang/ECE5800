54bd810c833299e8469b6163480a9a7899341fbd
rrrobinsonUI <rrrobnson@uiowa.edu>
2019-02-23 16:30:54 -0600

    Added login capabilities
    Not 100% sure on sessions yet, but password checking is there.

--- Group7/signup.php
+++ Group7/signup.php
@@ -6,11 +6,11 @@
          <section>
             <h1>SignUp</h1>
             <form action = "signup.inc.php" method = "post">
-               <input type="text" name="uid" placehold="Username">
-               <input type="text" name="mail" placehold="email">
-               <input type="password" name="pwd" placehold="Password">
-               <input type="password" name="pwd-r" placehold="Repeat Password">
-               <button type="submit" name="signup-submit">SignUp</button>
+               <input type="text" name="uid" placeholder="Username">
+               <input type="text" name="mail" placeholder="email">
+               <input type="password" name="pwd" placeholder="Password">
+               <input type="password" name="pwd-r" placeholder="Repeat Password">
+               <button type="submit" name="signup-submit">Sign Up</button>
          </section>
       </div>
     </main>
