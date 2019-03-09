9d45c96ae89af362a75a9b3dd851ad7e9a8dfbf7
zwang198 <zwang198@uiowa.edu>
2019-03-09 13:18:34 -0600

    few frontend work

--- Group7/account.php
+++ Group7/account.php
@@ -10,3 +10,22 @@
 <?php
     require "header.php"
 ?>
+<main>
+      <div class="container">
+         <section>
+            <h1>Account Settings</h1>
+
+            <form action = "account.inc.php" method = "post">
+            <div class="form-group"><input type="text" name="fname" placeholder="First Name"></div>
+            <div class="form-group"><input type="text" name="lname" placeholder="Last Name"></div>
+            <div class="form-group"><input type="text" name="Address" placeholder="Address"></div>
+            <div class="form-group"><input type="text" name="city" placeholder="city"></div>
+            <div class="form-group"><input type="text" name="state" placeholder="state"></div>
+            <div class="form-group"><input type="text" name="zip" placeholder="Zip Code"></div>
+            <div class="form-group"><input type="checkbox" name="userType" value="passenger"checked> I am a passenger<br>
+            <div class="form-group"><input type="checkbox" name="userType" value="driver" > I am a driver<br>
+            <div class="form-group"><button type="submit" class="btn btn-primary" name="signup-submit">SignUp</button></div>
+            </form>
+        </section>
+    </div>
+</main>
