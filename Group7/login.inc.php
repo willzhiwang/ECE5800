ddad742fabd9a15c9728faa0d8a73d58d13ab4ff
rrrobinsonUI <rrrobnson@uiowa.edu>
2019-03-31 00:13:15 -0500

    Updated login to work with new database

--- Group7/login.inc.php
+++ Group7/login.inc.php
@@ -23,7 +23,7 @@
         //check sql errors
         else
         {
-            $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
+            $sql = "SELECT * FROM account WHERE `Username`=? OR `Email`=?;";
             $stmt = mysqli_stmt_init($conn);
             //checking $sql statement error
             if (!mysqli_stmt_prepare($stmt,$sql))
@@ -33,23 +33,24 @@
             }
             else{
                 //pass in mailuid only not password yet
+                //Note: either username or e-mail may be used to log in
                 mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
                 mysqli_stmt_execute($stmt);
                 $result= mysqli_stmt_get_result($stmt);
                 //checking if the password matches the user
                 if ($row = mysqli_fetch_assoc($result))
                 {
-                    $pwdCheck = password_verify($password,$row['pwdUsers']);
-                    if (pwdCheck == false) // when match is false, wrong password
+                    $pwdCheck = password_verify($password,$row['Password']);
+                    if ($pwdCheck == false) // when match is false, wrong password
                     {
                         header("Location: login.php?error=wrongPWD");
                         exit();
                     }
-                    else if (pwdCheck == true) // approved login
+                    else if ($pwdCheck == true) // approved login
                     {
                         session_start();
-                        $_SESSION['userId'] = $row['idUsers'];
-                        $_SESSION['userUid'] = $row['uidUsers'];
+                        $_SESSION['userId'] = $row['ID'];
+                        $_SESSION['userUid'] = $row['Username'];
                         header("Location: index.php?login=success=.$mailuid");
                     }
                     else 
@@ -60,7 +61,7 @@
                 }
                 else{
    
-                    header("Location: index.php?error=success&user=".$mailuid);
+                    header("Location: login.php?error=nosuchuser");
                 }
             }
         }
