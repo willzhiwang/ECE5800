71a74225029b3a3c5ee7fbc3dd0185b2575cf606
PhillyDu <philadurose@gmail.com>
2019-04-01 09:52:25 -0500

    Update login.php
    Added front end to login page

--- Group7/login.php
+++ Group7/login.php
@@ -1,10 +1,9 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>group7</title>
+    <title>Group7</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!-- frontend work to make it looks better 
@@ -15,18 +14,67 @@
 <?php
     require "header.php"
 ?>
-    <main>
-    <div class="container">
-         <section>
-            <h1>Login</h1>
-            <form action = "login.inc.php" method = "post">
-            <div class="form-group"><input type="text" name="mail" placeholder="email"></div>
-            <div class="form-group"><input type="password" name="pwd" placeholder="Password"></div>
-            <div class="form-group"><button type="submit" class="btn btn-primary" name="login-submit">LogIn</button></div>
-         </section></div>
-      </div>
-    </main>
-    
+<body>
+	<header>
+		<nav class="navbar navbar-expand-sm" style="background-color:rgba(0,134,195,255)">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" style="color:rgba(255,255,255,255)" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" style="color:rgba(255,255,255,255);width:150px" href="costandsavings.php">Cost and Savings</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" style="color:rgba(255,255,255,255)" href="Map.php">Map</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" style="color:rgba(255,255,255,255);width:100px" href="fromto.php">From To</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" style="color:rgba(255,255,255,255)" href="search.php">Search</a>
+				</li>
+				<button type="button" class="btn bg-white" style="margin-left:1rem;color:rgba(0,0,0,255);width:120px">Contact Us</button>
+			</ul>
+		</nav>
+	<header>
+	<main>
+		<div class="container-fluid" style="background-image: linear-gradient(rgb(49,182,246),rgb(115,232,255)">
+		<br>
+		<br>
+			<div class="container bg-white" style="width:500px;height:800px;border-radius:25px;border-style:inset;border-width:large">
+				<section>
+					<h1 style="margin-left:10rem">Login</h1>
+					<br>
+						<form action="login.inc.php" method="post">
+							<div class="form-group" style="margin-left:8rem">
+								<label for="emailinput">Email</label>
+								<div class="row">
+									<div class="col-xs-3">
+										<input type="text" class="form-control" name="mail" id="emailinput" placeholder="Email">
+									</div>
+								</div>
+							</div>
+							<div class="form-group" style="margin-left:8rem">
+								<label for="passwordInput">Password</label>
+								<div class="row">
+									<div class="col-xs-3">
+										<input type="text" class="form-control" name="pwd" id="passwordInput" placeholder="Password">
+									</div>
+								</div>
+							</div>
+							<div class="form-group" style="margin-left:10rem">
+								<button type="submit" class="btn btn-lg btn-primary" name="login-submit">Login</button>
+							</div>
+							<br>
+						</form>
+				</section>
+			</div>
+		<br>
+		<br>
+		</div>
+	</main>
+</body>
+</html>
 <?php
     require "footer.php"
-?>   
+?>  
