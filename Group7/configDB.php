105efa69e2b4e9f7c8f47d0c8b3aa458a421ab43
zwang198 <zwang198@uiowa.edu>
2019-04-04 20:52:57 -0500

    fixed db name

--- Group7/configDB.php
+++ Group7/configDB.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "group7vanpool";
+$dbname = "Group7Vanpool";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
