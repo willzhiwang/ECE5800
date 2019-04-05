864822c75dc58c94370565acfca6330dc9fec887
rrrobinsonUI <42480761+rrrobinsonUI@users.noreply.github.com>
2019-04-05 17:38:53 -0500

    ID names more descriptive
    They now include the name of the table they're in, to make JOINs easier.

--- DocumentationEtc/CreateSQL.sql
+++ DocumentationEtc/CreateSQL.sql
@@ -9,20 +9,20 @@ CREATE DATABASE `Group7Vanpool`;
 USE `Group7Vanpool`;
 
 CREATE TABLE `Group7Vanpool`.`Address` ( 
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`AddressID` INT NOT NULL AUTO_INCREMENT , 
 `Name` VARCHAR(61) NULL,
 `Street` VARCHAR(127) NOT NULL , 
 `City` VARCHAR(31) NOT NULL , 
 `State` VARCHAR(2) NOT NULL , 
 `ZipCode` VARCHAR(10) NOT NULL ,  
-PRIMARY KEY (`ID`)
+PRIMARY KEY (`AddressID`)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE `Group7Vanpool`.`CardType` ( 
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`CardTypeID` INT NOT NULL AUTO_INCREMENT , 
 `CardName` VARCHAR(31) NOT NULL , 
-PRIMARY KEY (`ID`)
+PRIMARY KEY (`CardTypeID`)
 ) 
 ENGINE = InnoDB;
 
@@ -30,23 +30,23 @@ INSERT INTO `Group7Vanpool`.`CardType`(`CardName`) VALUES ("Mastercard");
 INSERT INTO `Group7Vanpool`.`CardType`(`CardName`) VALUES ("Visa");
 
 CREATE TABLE `Group7Vanpool`.`PaymentInfo` (
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`PaymentInfoID` INT NOT NULL AUTO_INCREMENT , 
 `CreditCardNumber` VARCHAR(255) NOT NULL , 
 `NameOnCard` VARCHAR(255) NOT NULL , 
 `ExpirationDate` VARCHAR(255) NOT NULL , 
 `SecurityCode` VARCHAR(255) NOT NULL , 
 `CardType` INT NOT NULL , 
 `BillingAddress` INT NOT NULL , 
-PRIMARY KEY (`ID`),
-FOREIGN KEY (`CardType`) REFERENCES `CardType`(`ID`),
-FOREIGN KEY (`BillingAddress`) REFERENCES `Address`(`ID`)
+PRIMARY KEY (`PaymentInfoID`),
+FOREIGN KEY (`CardType`) REFERENCES `CardType`(`CardTypeID`),
+FOREIGN KEY (`BillingAddress`) REFERENCES `Address`(`AddressID`)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE `Group7Vanpool`.`SecurityQuestion` ( 
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`SecurityQuestionID` INT NOT NULL AUTO_INCREMENT , 
 `Question` VARCHAR(255) NOT NULL ,
-PRIMARY KEY  (`ID`)
+PRIMARY KEY  (`SecurityQuestionID`)
 ) 
 ENGINE = InnoDB;
 
@@ -54,30 +54,30 @@ INSERT INTO `Group7Vanpool`.`SecurityQuestion`(`Question`) VALUES ("What is your
 INSERT INTO `Group7Vanpool`.`SecurityQuestion`(`Question`) VALUES ("What was the name of your elementary school?");
 
 CREATE TABLE `Group7Vanpool`.`User` (
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`UserID` INT NOT NULL AUTO_INCREMENT , 
 `IsDriver` BOOLEAN NOT NULL , 
 `IsAdmin` BOOLEAN NOT NULL ,
 `Balance` DECIMAL(12,2) NOT NULL , 
 `PaymentInfo` INT NULL , 
-PRIMARY KEY (`ID`),
-FOREIGN KEY (`PaymentInfo`) REFERENCES `PaymentInfo`(`ID`)
+PRIMARY KEY (`UserID`),
+FOREIGN KEY (`PaymentInfo`) REFERENCES `PaymentInfo`(`PaymentInfoID`)
 ) 
 ENGINE = InnoDB;
 
 CREATE TABLE `Group7Vanpool`.`Person` ( 
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`PersonID` INT NOT NULL AUTO_INCREMENT , 
 `FirstName` VARCHAR(20) NULL , 
 `LastName` VARCHAR(20) NULL , 
 `DateOfBirth` DATE NULL , 
 `PhoneNumber` VARCHAR(16) NULL ,
 `Address` INT NULL , 
-PRIMARY KEY (`ID`),
-FOREIGN KEY (`Address`) REFERENCES `Address`(`ID`)
+PRIMARY KEY (`PersonID`),
+FOREIGN KEY (`Address`) REFERENCES `Address`(`AddressID`)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE `Group7Vanpool`.`Account` ( 
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`AccountID` INT NOT NULL AUTO_INCREMENT , 
 `Email` VARCHAR(64) NOT NULL , 
 `Username` VARCHAR(20) NOT NULL , 
 `Password` VARCHAR(255) NOT NULL , 
@@ -89,29 +89,29 @@ CREATE TABLE `Group7Vanpool`.`Account` (
 `SecAnswer3` VARCHAR(50) NULL ,
 `UserID` INT NOT NULL,
 `PersonID` INT NULL,
-PRIMARY KEY (`ID`),
-FOREIGN KEY (`SecQuestion1`) REFERENCES `SecurityQuestion`(`ID`),
-FOREIGN KEY (`SecQuestion2`) REFERENCES `SecurityQuestion`(`ID`),
-FOREIGN KEY (`SecQuestion3`) REFERENCES `SecurityQuestion`(`ID`),
-FOREIGN KEY (`UserID`) REFERENCES `User`(`ID`),
-FOREIGN KEY (`PersonID`) REFERENCES `Person`(`ID`)
+PRIMARY KEY (`AccountID`),
+FOREIGN KEY (`SecQuestion1`) REFERENCES `SecurityQuestion`(`SecurityQuestionID`),
+FOREIGN KEY (`SecQuestion2`) REFERENCES `SecurityQuestion`(`SecurityQuestionID`),
+FOREIGN KEY (`SecQuestion3`) REFERENCES `SecurityQuestion`(`SecurityQuestionID`),
+FOREIGN KEY (`UserID`) REFERENCES `User`(`UserID`),
+FOREIGN KEY (`PersonID`) REFERENCES `Person`(`PersonID`)
 ) 
 ENGINE = InnoDB;
 
 CREATE TABLE `Group7Vanpool`.`Vehicle` ( 
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`VehicleID` INT NOT NULL AUTO_INCREMENT , 
 `OwnerUser` INT NULL , 
 `LicensePlate` VARCHAR(9) NOT NULL , 
 `Year` VARCHAR(5) NOT NULL , 
 `Model` VARCHAR(31) NOT NULL , 
 `Color` VARCHAR(15) NOT NULL , 
-PRIMARY KEY (`ID`),
-FOREIGN KEY (`OwnerUser`) REFERENCES `User`(`ID`)
+PRIMARY KEY (`VehicleID`),
+FOREIGN KEY (`OwnerUser`) REFERENCES `User`(`UserID`)
 ) 
 ENGINE = InnoDB;
 
 CREATE TABLE `Group7Vanpool`.`Route` ( 
-`ID` INT NOT NULL AUTO_INCREMENT , 
+`RouteID` INT NOT NULL AUTO_INCREMENT , 
 `Vehicle` INT NOT NULL , 
 `FromAddress` INT NOT NULL , 
 `ToAddress` INT NOT NULL , 
@@ -121,10 +121,10 @@ CREATE TABLE `Group7Vanpool`.`Route` (
 `MileDistance` DECIMAL(6,2) NOT NULL ,
 `MaxCapacity` INT NOT NULL ,
 `CurrCapacity` INT NOT NULL , 
-PRIMARY KEY (`ID`),
-FOREIGN KEY (`Vehicle`) REFERENCES `Vehicle`(`ID`),
-FOREIGN KEY (`FromAddress`) REFERENCES `Address`(`ID`),
-FOREIGN KEY (`ToAddress`) REFERENCES `Address`(`ID`)
+PRIMARY KEY (`RouteID`),
+FOREIGN KEY (`Vehicle`) REFERENCES `Vehicle`(`VehicleID`),
+FOREIGN KEY (`FromAddress`) REFERENCES `Address`(`AddressID`),
+FOREIGN KEY (`ToAddress`) REFERENCES `Address`(`AddressID`)
 )
 ENGINE = InnoDB;
 
@@ -132,8 +132,8 @@ CREATE TABLE `Group7Vanpool`.`PassengerToRoutes` (
 `UserID` INT NOT NULL , 
 `RouteID` INT NOT NULL , 
 PRIMARY KEY (`UserID`, `RouteID`) ,
-FOREIGN KEY (`UserID`) REFERENCES `User`(`ID`),
-FOREIGN KEY (`RouteID`) REFERENCES `Route`(`ID`)
+FOREIGN KEY (`UserID`) REFERENCES `User`(`UserID`),
+FOREIGN KEY (`RouteID`) REFERENCES `Route`(`RouteID`)
 )
 ENGINE = InnoDB;
 
@@ -141,7 +141,7 @@ CREATE TABLE `Group7Vanpool`.`DriverToRoutes` (
 `UserID` INT NOT NULL , 
 `RouteID` INT NOT NULL , 
 PRIMARY KEY (`UserID`, `RouteID`) ,
-FOREIGN KEY (`UserID`) REFERENCES `User`(`ID`),
-FOREIGN KEY (`RouteID`) REFERENCES `Route`(`ID`)
+FOREIGN KEY (`UserID`) REFERENCES `User`(`UserID`),
+FOREIGN KEY (`RouteID`) REFERENCES `Route`(`RouteID`)
 )
 ENGINE = InnoDB;
